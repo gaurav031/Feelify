8a60df4e0fc577360860f17a9da315c3a7f2b941
gaurav031 <gauravraaz2005@gmail.com>
2024-11-05 13:38:27 +0530

    update server

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,23 +1,21 @@
 import cron from "cron";
-import http from "http"; // Import http for HTTP requests
-import https from "https"; // Import https for HTTPS requests
+import https from "https";
 
-const URL = "https://localhost:3000"; // Change to HTTPS if your server supports it
+const URL = "http://localhost:3000";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
-    const protocol = URL.startsWith("https") ? https : http; // Determine which module to use based on the URL
-
-    protocol
-        .get(URL, (res) => {
-            if (res.statusCode === 200) {
-                console.log("GET request sent successfully");
-            } else {
-                console.log("GET request failed", res.statusCode);
-            }
-        })
-        .on("error", (e) => {
-            console.error("Error while sending request", e);
-        });
+	https
+		.get(URL, (res) => {
+			if (res.statusCode === 200) {
+				console.log("GET request sent successfully");
+			} else {
+				console.log("GET request failed", res.statusCode);
+			}
+		})
+		.on("error", (e) => {
+			console.error("Error while sending request", e);
+		});
 });
 
 export default job;
+
